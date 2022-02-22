7943a50df92175ca07cbc626d62bfa7d75073510
Kep <KepCreations@gmail.com>
2022-02-22 00:25:42 -0500

    Clean up Homepage

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { Link as RouterLink, useHistory, Redirect } from 'react-router-dom';
 import { clearUserPin, fetchUserFromAPI, clockInUser } from '../actions/user';
-import TapntableApi from '../api/api';
 import { Typography, Link, Button } from '@mui/material';
 import UserPinForm from '../auth/UserPinForm';
 import {
@@ -25,23 +24,11 @@ const Homepage = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // const [ showUserPinForm, setShowUserPinForm ] = useState(true);
-  const [ showClockOut, setShowClockOut ] = useState(false);
-  // const [ showClockIn, setShowClockIn ] = useState(true);
-
   const getUser = ({ pin }) => {
     console.log('login', pin);
 
     dispatch(fetchUserFromAPI(pin));
     console.log('User Id: ', user);
-
-    // if (user.isClockedIn && user.roleId !== 2) history.push('/servers');
-    // if (user.isClockedIn) {
-    //   history.push('/clockout');
-    // } else {
-    //   console.log('Punch in only');
-    //   dispatch(clearUserPin());
-    // }
   };
 
   const clockIn = async (userId) => {
@@ -107,43 +94,9 @@ const Homepage = () => {
         </div>
       );
     }
-    return (
-      <div>
-        <Typography variant="h3" align="center">
-          Tapntable
-        </Typography>
-        {!showClockOut && (
-          <div>
-            <Button
-              onClick={() => clockIn(user.id)}
-              variant="contained"
-              align="center"
-            >
-              Clock In
-            </Button>
-            <Button onClick={cancelClockIn} variant="contained" align="center">
-              Cancel
-            </Button>
-          </div>
-        )}
-        {showClockOut && <ClockOut />}
-      </div>
-    );
   }
 
-  return (
-    <div>
-      <Typography variant="h3" align="center">
-        Tapntable
-      </Typography>
-
-      <Typography variant="h4" align="center">
-        <Button variant="contained" component={RouterLink} to="/server">
-          Server Page
-        </Button>
-      </Typography>
-    </div>
-  );
+  return <Redirect to="/servers" />;
 
   // return (
   //   <div>

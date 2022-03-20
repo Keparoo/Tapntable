27697125152d62ffcd7c52d5eef90b95f3f606ae
Kep <KepCreations@gmail.com>
2022-03-19 23:07:59 -0400

    Add 86 and count items to Welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,15 +1,29 @@
-import { Typography, Button, Container, Stack, Paper } from '@mui/material';
+import {
+  Typography,
+  Button,
+  Container,
+  Stack,
+  Paper,
+  List,
+  ListItem
+} from '@mui/material';
 import React from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { clearUserPin } from '../actions/user';
 import { isClockInOnly } from '../utils/helpers';
 import restaurantConfig from '../restaurantConfig.json';
+import { Box } from '@mui/system';
 
 const Welcome = () => {
   const user = useSelector((state) => state.user, shallowEqual);
+  const items = useSelector((state) => state.items, shallowEqual);
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const items86 = items.filter((i) => i.count === 0);
+  const itemsWithCount = items.filter((i) => i.count > 0);
+  console.log('86 List', items86, itemsWithCount);
 
   const handleClick = () => {
     if (isClockInOnly(user.role)) {
@@ -29,7 +43,41 @@ const Welcome = () => {
             Welcome {user.displayName} to {restaurantConfig.restaurant.name}.<br />
             Have a great shift!
           </Typography>
-
+          <br />
+          <Typography variant="h4" align="center">
+            86 List
+          </Typography>
+          {items86.length &&
+            items86.map((i) => (
+              <Box
+                sx={{
+                  width: '300px',
+                  marginLeft: '300px'
+                }}
+              >
+                <List>
+                  <ListItem>{i.name}</ListItem>
+                </List>
+              </Box>
+            ))}
+          <Typography variant="h4" align="center">
+            Items with Count
+          </Typography>
+          {itemsWithCount.length &&
+            itemsWithCount.map((i) => (
+              <Box
+                sx={{
+                  width: '300px',
+                  marginLeft: '300px'
+                }}
+              >
+                <List>
+                  <ListItem>
+                    {i.name} {i.count}
+                  </ListItem>
+                </List>
+              </Box>
+            ))}
           <br />
           <Stack>
             <Button variant="contained" onClick={handleClick}>
